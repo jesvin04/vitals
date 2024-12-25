c2ee2a11948619f8cff722ab093b4f8523e704f2
jesvin04 <jesvin5021@gmail.com>
2024-12-26 00:10:42 +0530

    fixed broken import of navigate  in app.jsx

--- vitals/src/App.jsx
+++ vitals/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage/LoginPage';
 import ShoppingList from './ShoppingList/ShoppingList';
 import SignupPage from "./signuppage/SignupPage"
